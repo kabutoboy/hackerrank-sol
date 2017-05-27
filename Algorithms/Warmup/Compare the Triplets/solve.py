911b295ad948c07b24e1ea57ecfc7bc8f91b6623
aum <kabutoboy@gmail.com>
2017-05-27 23:27:59 +0700

    add sol compare the triplets

---
+++ Algorithms/Warmup/Compare the Triplets/solve.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+
+def score(A, B):
+    sA, sB = 0, 0
+    for a, b in zip(A, B):
+    if a > b:
+        sA += 1
+    elif b > a:
+        sB += 1
+    return sA, sB
+
+def solve(a0, a1, a2, b0, b1, b2):
+    return score([a0, a1, a2], [b0, b1, b2])
+
+a0, a1, a2 = input().strip().split(' ')
+a0, a1, a2 = [int(a0), int(a1), int(a2)]
+b0, b1, b2 = input().strip().split(' ')
+b0, b1, b2 = [int(b0), int(b1), int(b2)]
+result = solve(a0, a1, a2, b0, b1, b2)
+print (' '.join(map(str, result)))
+
